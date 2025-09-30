f4248fa6ba0786f4c9e97a1f86ceb411ec6ba6b0
syahjr <syahrifan2007@gmail.com>
2025-09-30 15:01:52 +0800

    Menu Terbaru

--- FormMenu.Designer.cs
+++ FormMenu.Designer.cs
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.formMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formSiswaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.formMenuToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // formMenuToolStripMenuItem
+            // 
+            this.formMenuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.formSiswaToolStripMenuItem});
+            this.formMenuToolStripMenuItem.Name = "formMenuToolStripMenuItem";
+            this.formMenuToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.formMenuToolStripMenuItem.Text = "Form Menu";
+            // 
+            // formSiswaToolStripMenuItem
+            // 
+            this.formSiswaToolStripMenuItem.Name = "formSiswaToolStripMenuItem";
+            this.formSiswaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.formSiswaToolStripMenuItem.Text = "Form Siswa";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // FormMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMenu";
             this.Text = "FormMenu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem formMenuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formSiswaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
