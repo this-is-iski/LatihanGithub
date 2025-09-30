e333f4b1925dbcd2c56da94b00138623678b2294
ragahaidari026 <ragahaidari026@gmail.com>
2025-09-30 15:09:13 +0800

    Form Siswa #1 dari Raga :)

---
+++ FormSiswa.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace LatihanGithub
+{
+    partial class FormSiswa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbNama
+            // 
+            this.tbNama.Location = new System.Drawing.Point(313, 62);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(212, 26);
+            this.tbNama.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nama Siswa";
+            // 
+            // FormSiswa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbNama);
+            this.Name = "FormSiswa";
+            this.Text = "FormSiswa";
+            this.Load += new System.EventHandler(this.FormSiswa_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.Label label1;
+    }
+}
