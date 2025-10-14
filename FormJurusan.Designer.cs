93b20334d006e6412161164a763f90bd0bc7f5e8
this-is-iski <muhammadiski779@gmail.com>
2025-10-14 15:26:37 +0800

    tes

--- FormJurusan.Designer.cs
+++ FormJurusan.Designer.cs
@@ -43,35 +43,38 @@
             // lblidjurusan
             // 
             this.lblidjurusan.AutoSize = true;
-            this.lblidjurusan.Location = new System.Drawing.Point(103, 112);
+            this.lblidjurusan.Location = new System.Drawing.Point(92, 90);
             this.lblidjurusan.Name = "lblidjurusan";
-            this.lblidjurusan.Size = new System.Drawing.Size(87, 20);
+            this.lblidjurusan.Size = new System.Drawing.Size(70, 16);
             this.lblidjurusan.TabIndex = 0;
             this.lblidjurusan.Text = "ID Jurusan";
             // 
             // lbljurusan
             // 
             this.lbljurusan.AutoSize = true;
-            this.lbljurusan.Location = new System.Drawing.Point(124, 170);
+            this.lbljurusan.Location = new System.Drawing.Point(110, 136);
             this.lbljurusan.Name = "lbljurusan";
-            this.lbljurusan.Size = new System.Drawing.Size(66, 20);
+            this.lbljurusan.Size = new System.Drawing.Size(54, 16);
             this.lbljurusan.TabIndex = 1;
             this.lbljurusan.Text = "Jurusan";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(90, 406);
+            this.button1.Location = new System.Drawing.Point(80, 325);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.Size = new System.Drawing.Size(67, 34);
             this.button1.TabIndex = 2;
             this.button1.Text = "Simpan";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnedit
             // 
-            this.btnedit.Location = new System.Drawing.Point(221, 406);
+            this.btnedit.Location = new System.Drawing.Point(196, 325);
+            this.btnedit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnedit.Name = "btnedit";
-            this.btnedit.Size = new System.Drawing.Size(75, 42);
+            this.btnedit.Size = new System.Drawing.Size(67, 34);
             this.btnedit.TabIndex = 3;
             this.btnedit.Text = "Edit";
             this.btnedit.UseVisualStyleBackColor = true;
@@ -79,34 +82,38 @@
             // 
             // txtidjurusan
             // 
-            this.txtidjurusan.Location = new System.Drawing.Point(221, 109);
+            this.txtidjurusan.Location = new System.Drawing.Point(196, 87);
+            this.txtidjurusan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtidjurusan.Name = "txtidjurusan";
-            this.txtidjurusan.Size = new System.Drawing.Size(221, 26);
+            this.txtidjurusan.Size = new System.Drawing.Size(197, 22);
             this.txtidjurusan.TabIndex = 4;
             // 
             // txtjurusan
             // 
-            this.txtjurusan.Location = new System.Drawing.Point(221, 164);
+            this.txtjurusan.Location = new System.Drawing.Point(196, 131);
+            this.txtjurusan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtjurusan.Name = "txtjurusan";
-            this.txtjurusan.Size = new System.Drawing.Size(221, 26);
+            this.txtjurusan.Size = new System.Drawing.Size(197, 22);
             this.txtjurusan.TabIndex = 5;
             // 
             // dgjurusan
             // 
             this.dgjurusan.BackgroundColor = System.Drawing.SystemColors.Info;
             this.dgjurusan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgjurusan.Location = new System.Drawing.Point(90, 221);
+            this.dgjurusan.Location = new System.Drawing.Point(80, 177);
+            this.dgjurusan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgjurusan.Name = "dgjurusan";
             this.dgjurusan.RowHeadersWidth = 62;
             this.dgjurusan.RowTemplate.Height = 28;
-            this.dgjurusan.Size = new System.Drawing.Size(352, 150);
+            this.dgjurusan.Size = new System.Drawing.Size(313, 120);
             this.dgjurusan.TabIndex = 6;
             // 
             // btnhapus
             // 
-            this.btnhapus.Location = new System.Drawing.Point(367, 406);
+            this.btnhapus.Location = new System.Drawing.Point(326, 325);
+            this.btnhapus.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnhapus.Name = "btnhapus";
-            this.btnhapus.Size = new System.Drawing.Size(75, 42);
+            this.btnhapus.Size = new System.Drawing.Size(67, 34);
             this.btnhapus.TabIndex = 7;
             this.btnhapus.Text = "Hapus";
             this.btnhapus.UseVisualStyleBackColor = true;
@@ -115,18 +122,18 @@
             // 
             this.lblformjurusan.AutoSize = true;
             this.lblformjurusan.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblformjurusan.Location = new System.Drawing.Point(183, 37);
+            this.lblformjurusan.Location = new System.Drawing.Point(163, 30);
             this.lblformjurusan.Name = "lblformjurusan";
-            this.lblformjurusan.Size = new System.Drawing.Size(186, 32);
+            this.lblformjurusan.Size = new System.Drawing.Size(158, 26);
             this.lblformjurusan.TabIndex = 8;
             this.lblformjurusan.Text = "Form Jurusan";
             // 
             // FormJurusan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(543, 505);
+            this.ClientSize = new System.Drawing.Size(483, 404);
             this.Controls.Add(this.lblformjurusan);
             this.Controls.Add(this.btnhapus);
             this.Controls.Add(this.dgjurusan);
@@ -136,6 +143,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lbljurusan);
             this.Controls.Add(this.lblidjurusan);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormJurusan";
             this.Text = "FormJurusan";
             ((System.ComponentModel.ISupportInitialize)(this.dgjurusan)).EndInit();
--- FormJurusan.cs
+++ FormJurusan.cs
@@ -21,5 +21,10 @@ namespace LatihanGithub
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
